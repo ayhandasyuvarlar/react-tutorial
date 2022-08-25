440dcc866c2eca5c4daa03bdb1d32d35158587a1
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-08-25 19:36:47 +0300

    added Bootstrap

--- react-app/src/components/User.jsx
+++ react-app/src/components/User.jsx
@@ -2,9 +2,9 @@ const User = ({ name, situation  }) => {
   return (
    <div className="col-12">
      <div className="card mt-1 p-5 w-100">
+     <button className="btn btn-danger" style={{position:'absolute' , right:'10px' ,top:'5px'}}>Sil</button>
       <h3>{name ? 'Merhaba ': ''} İsmim {name}</h3>
       <h3 className="situation">Durum : {situation}</h3>
-      <button className="btn btn-dark">Durumu Guncelle</button>
     </div>
    </div>
   );
