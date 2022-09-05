4903e6700f9e17de752dd755fe0bc24112807463
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-09-05 18:40:57 +0300

    directly process

--- countries-app/src/App.js
+++ countries-app/src/App.js
@@ -1,9 +1,24 @@
-
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>asdas</div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div>
+          <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/About" element={<About/>} /> 
+          <Route path="/Contact" element={<Contact/>} /> 
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
